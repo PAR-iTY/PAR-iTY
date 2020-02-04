a763ac2e0274e6c5ff50760c03748c4a3622bcee
PAR-iTY <44795502+PAR-iTY@users.noreply.github.com>
2020-02-04 16:00:42 +1300

    clean first jim push

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // NEW IDEA: WEB AUDIO / WEB CAM --> EMOJI-CANNON OF UDP TERMINAL DATA
 // fits with new IG idea: UDP_EMOJI_CANNON / asciicannon / UDP_emoji
 // wierd new idea: reveal primes on page on.load with some sort of
@@ -119,7 +118,7 @@ incredible way of 'stealing' non DOM information from CSS psuedo-content
 
 // tar them with black paint!
 // console.log(children[heretic]);
-=======
+
 // NEW IDEA: WEB AUDIO / WEB CAM --> EMOJI-CANNON OF UDP TERMINAL DATA
 // fits with new IG idea: UDP_EMOJI_CANNON / asciicannon / UDP_emoji
 // wierd new idea: reveal primes on page on.load with some sort of
@@ -130,12 +129,12 @@ incredible way of 'stealing' non DOM information from CSS psuedo-content
 
 // NEXT IDEA: change 'base' to 'col' / 'column' b/c it base is a lie lmao
 
-const tiles = document.querySelectorAll(".tile"),
-  col = document.getElementById("col"),
-  floor = document.querySelector(".floor");
+const tiles = document.querySelectorAll('.tile'),
+  col = document.getElementById('col'),
+  floor = document.querySelector('.floor');
 
 // catch earliest input to validate and handle exceptions
-col.addEventListener("keydown", e => {
+col.addEventListener('keydown', e => {
   // initial value of DOM element
   const v1 = e.target.innerHTML,
     reg = /^\d+$/;
@@ -144,32 +143,29 @@ col.addEventListener("keydown", e => {
 
   // console.log(e.cancelable); // true
 
-  if (e.key === "Backspace" || e.key === "Delete") {
+  if (e.key === 'Backspace' || e.key === 'Delete') {
     // allow special keys: delete, backspace, ...?
     isInt = false;
   }
 
   // safety check if #col is about to be empty (representing zero columns)
   if (v1.length === 1 && isInt === false) {
-    floor.setAttribute(
-      "style",
-      "grid-template-columns: repeat(1, 1fr) !important;"
-    );
+    floor.setAttribute('style', 'grid-template-columns: repeat(1, 1fr) !important;');
   }
 
   // if ANY OTHER KEYDOWN OCCURS: run regex blocker
   if (isInt === true && reg.test(e.key) === false) {
-    console.log("isInt = false", e.key);
+    console.log('isInt = false', e.key);
     // prevents event bubbling to 'input' below
     e.preventDefault();
   } else {
-    console.log("isInt = true", e.key);
+    console.log('isInt = true', e.key);
   }
 });
 
 // if input reaches this later listener and is less than n columns, apply currently displayed value
-col.addEventListener("input", () => {
-  console.log("reaches input");
+col.addEventListener('input', () => {
+  console.log('reaches input');
   // why does console.log(e.key); return undefined here? is 'input' not technically a keyboardEvent?
   // is the event being blocked by regex tests preventDefault? might looking into bubbling help? (ughhhh)
 
@@ -179,10 +175,7 @@ col.addEventListener("input", () => {
   // if, for sanity, user input is within an arbitrary n apply column change
   if (colInt < 151) {
     // hardcode set columns (due to media queries and !importants in css file... cleanup !importants !!)
-    floor.setAttribute(
-      "style",
-      `grid-template-columns: repeat(${colInt}, 1fr) !important;`
-    );
+    floor.setAttribute('style', `grid-template-columns: repeat(${colInt}, 1fr) !important;`);
   }
 });
 
@@ -194,25 +187,25 @@ let pinkC = 0,
   pinkyC = 0;
 
 // pink tile listener
-tiles[1].addEventListener("click", () => {
+tiles[1].addEventListener('click', () => {
   // bit test if even --> add
   if ((pinkC & 1) === 0) {
-    toggleTiles("pink", "add");
+    toggleTiles('pink', 'add');
   } else {
     // is odd --> remove
-    toggleTiles("pink", "remove");
+    toggleTiles('pink', 'remove');
   }
   pinkC++;
 });
 
 // pinky tile listener
-tiles[2].addEventListener("click", () => {
+tiles[2].addEventListener('click', () => {
   // bit test if even --> add
   if ((pinkyC & 1) === 0) {
-    toggleTiles("pinky", "add");
+    toggleTiles('pinky', 'add');
   } else {
     // is odd --> remove
-    toggleTiles("pinky", "remove");
+    toggleTiles('pinky', 'remove');
   }
   pinkyC++;
 });
@@ -220,10 +213,10 @@ tiles[2].addEventListener("click", () => {
 // generic tile loop accepts a color[class name] and an action
 function toggleTiles(color, action) {
   tiles.forEach(tile => {
-    if (action === "add") {
+    if (action === 'add') {
       tile.classList.add(color);
     }
-    if (action === "remove") {
+    if (action === 'remove') {
       tile.classList.remove(color);
     }
   });
@@ -247,4 +240,3 @@ incredible way of 'stealing' non DOM information from CSS psuedo-content
 
 // tar them with black paint!
 // console.log(children[heretic]);
->>>>>>> f842146f3fa58992fba65baab5c85e1f462cd6ef
